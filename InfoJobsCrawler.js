3725d9fb66dbe990e11c40b2f37764a20e6eac6b
Chris_Sharp <82599146+ChrisSharpDotEs@users.noreply.github.com>
2025-04-05 20:29:31 +0100

    Update InfoJobsCrawler.js

--- InfoJobsCrawler.js
+++ InfoJobsCrawler.js
@@ -1,4 +1,8 @@
-/*Copia y pega este código en tu consola y obtendrás la lista de aplicaciones en infojobs extrayendo la información relevante*/
+/**
+* Copia y pega este código en tu consola y obtendrás la lista de inscripciones en ofertas a las 
+* que te has inscrito en infojobs extrayendo la información relevante como:
+* Nombre de la empresa, Nombre del puesto, fecha en la que te inscribiste y el estado
+*/
 [...document.querySelectorAll('li')].filter(item => item.getAttribute('id')).map(item => {
     return {
         "Puesto": item.querySelector('h2 span').innerHTML,
